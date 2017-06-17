cae3fe6dcc817a6a07852623106f0cd7e8da40f6
Joseph H Kennedy <kennedyjh@ornl.gov>
2017-06-16 22:40:44 -0400

    Add metadata check

--- atlas.py
+++ atlas.py
@@ -8,6 +8,8 @@ import six
 import os
 import subprocess
 
+from netCDF4 import Dataset
+
 import livvkit
 from livvkit.util import elements as EL
 from livvkit.util import functions as FN
@@ -25,20 +27,16 @@ def run(mip_name, config):
     Runs the analysis.
 
     Args:
-        name: The name of the test
+        mip_name: The name of the test
         config: A dictionary representation of the configuration file
 
     Returns:
-       The result of elements.page with the list of elements to display
+       The result of elements.book with the list of pages to display
     """
   
     mip = mip_config(mip_name)
     img_dir = os.path.join(livvkit.output_dir, 'validation', 'imgs', mip_name)
     FN.mkdir_p(img_dir)
-
-    exp_img = {'init':'init_topg_test.png', 
-               'ctrl':'ctrl_topg_test.png', 
-               'asmb':'asmb_topg_test.png'}
 
     pages = {}
     for group in config['groups']:
@@ -46,22 +44,32 @@ def run(mip_name, config):
             tabs = []
             for exp in config['experiments']:
                 images = []
+                err_msg = []
                 for var in mip:
                     ice_sheet = mip_name.split('-')[-1]
-                    img_name = '_'.join([var, ice_sheet, group, model, exp]) + '.png'
-                    img_file = os.path.relpath(os.path.join(img_dir, img_name), os.getcwd())
-                  
+                    
                     data_dir = os.path.join(os.path.abspath(config['data_path']), group, model, exp)
                     data_name = '_'.join([var, ice_sheet, group, model, exp]) + '.nc'
                     data_file = os.path.relpath(os.path.join(data_dir, data_name), os.getcwd())
 
-                    plot_var(config['plot_script'], data_file, img_file, exp, var, mip)
-                    images.append(EL.image(var, '', '/'.join([mip_name, img_name]) ))
+                    err_msg.extend(check_meta(data_file, var, mip))
+                    
+                    if var != 'scalar':
+                        img_name = '_'.join([var, ice_sheet, group, model, exp]) + '.png'
+                        img_file = os.path.relpath(os.path.join(img_dir, img_name), os.getcwd())
+                      
+                        plot_var(config['plot_script'], data_file, img_file, exp, var, mip)
+                        images.append(EL.image(var, '', '/'.join([mip_name, img_name]) ))
 
                 elements = []
-                elements.append(EL.gallery('Var gallery', images))
-                elements.append(EL.error('Meta check', 'To Implement: Metadata check.'))
+                if not err_msg:
+                    err_msg_str = 'None<br><p style="color:black">Everything looks good!</p>'
+                else:
+                    err_msg_str = '<br>' + '<br><br>'.join(err_msg)
+                elements.append(EL.error('Meta check', err_msg_str))
                 
+                elements.append(EL.gallery('Var gallery', images))
+               
                 tabs.append(EL.tab(exp, element_list=elements))
 
             page_name = '-'.join([group, model])
@@ -72,17 +80,17 @@ def run(mip_name, config):
 
 def plot_var(plot_script, data_file, img_file, exp, var, mip):
     ncl_command = ' '.join(["ncl", "-Q",
-                            "'afile=\"" + data_file + "\"'",
-                            "'ofile=\"" + img_file + "\"'",
-                            "'aexp=\"" + exp + "\"'",
-                            "'avar=\"" + var + "\"'",
-                            "'atsp=" + str(mip[var]['timestep'][exp]) + "'",
-                            "'apal=\"" + mip[var]['palette'] + "\"'",
-                            "'amod=" + str(mip[var]['levelmode']) + "'",
-                            "'amin=" + str(mip[var]['lmin']) + "'",
-                            "'amax=" + str(mip[var]['lmax']) + "'",
-                            "'alsp=" + str(mip[var]['lstep']) + "'",
-                            "'alvl=" + mip[var]['levels'] + "'",
+                            "'afile=\"{}\"'".format(data_file),
+                            "'ofile=\"{}\"'".format(img_file),
+                            "'aexp=\"{}\"'".format(exp),
+                            "'avar=\"{}\"'".format(var),
+                            "'atsp={}'".format(mip[var]['timestep'][exp]),
+                            "'apal=\"{}\"'".format(mip[var]['palette']),
+                            "'amod={}'".format(mip[var]['levelmode']),
+                            "'amin={}'".format(mip[var]['lmin']),
+                            "'amax={}'".format(mip[var]['lmax']),
+                            "'alsp={}'".format(mip[var]['lstep']),
+                            "'alvl={}'".format(mip[var]['levels']),
                             plot_script])
                             
     proc = subprocess.Popen(ncl_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -92,6 +100,65 @@ def plot_var(plot_script, data_file, img_file, exp, var, mip):
         with open(ncl_errors.txt, 'a') as f:
             f.write(ncl_command)
             f.write(pout)  
+
+def check_meta(data_file, var, mip):
+    message = []
+    if not os.path.exists(data_file):
+        msg = '{} file missing: <br> &emsp; {}'.format(var, data_file)
+        message.append(msg)
+        return message
+    else:
+        try:
+            nc_var = Dataset(data_file, 'r')
+        except:
+            msg = '{} file could not be read: <br> &emsp; {}'.format(var, data_file)
+            message.append(msg)
+            return message
+
+    if var == 'scalar':
+        message.extend(check_scalar_meta(nc_var, mip[var]))
+    else:
+        meta = mip[var]['meta']
+        var_data = nc_var.variables[var] 
+        ncattr = var_data.ncattrs()
+        if 'standard_name' not in ncattr:
+            message.append('{} missing attribute: "standard_name" '.format(var))
+        elif var_data.getncattr('standard_name') != meta['standard_name']:
+            message.append('{} standard name is "{}" but  it should be "{}" '.format(
+                               var, var_data.getncattr('standard_name'), meta['standard_name']))
+
+        if 'units' not in ncattr:
+            message.append('{} missing attribute: "units" '.format(var))
+
+        ndims = len(var_data[:].shape)
+        if ndims != len(meta['dims']):
+            message.append('{} has  {} dimensions but it should have {} '.format(var, ndims, len(meta['dims'])))
+
+    return message
+
+
+def check_scalar_meta(nc_var, scalars):
+    message = []
+    for var, details in six.iteritems(scalars):
+        meta = details['meta']
+        var_data = nc_var.variables[var] 
+        ncattr = var_data.ncattrs()
+        print(ncattr)
+        if 'standard_name' not in ncattr:
+            message.append('scalar {} missing attribute: "standard_name" '.format(var))
+        elif var_data.getncattr('standard_name') != meta['standard_name']:
+            message.append('scalar {} standard name is "{}" but  it should be "{}" '.format(
+                               var, var_data.getncattr('standard_name'), meta['standard_name']))
+
+        if 'units' not in ncattr:
+            message.append('scalar {} missing attribute: "units" '.format(var))
+
+        ndims = len(var_data[:].shape)
+        if ndims != len(meta['dims']):
+            message.append('scalar {} has  {} dimensions but it should have {} '.format(var, ndims, len(meta['dims'])))
+
+    return message
+
 
 
 def print_summary(case, summary):
